90d9dd036b534785da0f4b5049e4e7cb8aa4d2df
SirAxLord <A325641@alumnos.uaslp.mx>
2025-02-17 02:02:34 -0600

    Se agrego que el enemigo siga al personaje, y una trampa que de momento imprime en la consola que el jugador esta herido

--- juego_ua_arfr/Scripts/Jugador/character.gd
+++ juego_ua_arfr/Scripts/Jugador/character.gd
@@ -1,19 +1,23 @@
-class_name Character
+class_name character
 extends CharacterBody2D
-
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const MAX_JUMPS = 2
 
+var jump_count = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		jump_count = 0  # Restablecer el contador de saltos cuando esté en el suelo
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and jump_count < MAX_JUMPS:
 		velocity.y = JUMP_VELOCITY
+		jump_count += 1  # Incrementar el contador de saltos
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
