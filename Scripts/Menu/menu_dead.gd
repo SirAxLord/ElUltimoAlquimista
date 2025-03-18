54b2df32e6036c14fc749e639065f6206d8a99b6
Adrian-Flores-269990 <100820539+Adrian-Flores-269990@users.noreply.github.com>
2025-03-17 23:05:24 -0600

    Delete Scripts directory

--- Scripts/Menu/menu_dead.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _on_reiniciar_pressed() -> void:
-	get_tree().change_scene_to_file("res://Levels/level_prueba.tscn")
-
-
-func _on_salir_pressed() -> void:
-	get_tree().change_scene_to_file("res://Escenas/Menus/Menu_Principal.tscn")
