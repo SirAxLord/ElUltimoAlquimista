0edbb27afc691fa2e1bc40d9bb1ef28526357219
SirAxLord <A325641@alumnos.uaslp.mx>
2025-03-22 22:04:12 -0600

    Integracion de cambios

--- juego_ua_arfr/Scripts/Jugador/character.gd
+++ juego_ua_arfr/Scripts/Jugador/character.gd
@@ -8,6 +8,7 @@ extends CharacterBody2D
 var can_double_jump := true  # Permite controlar si el personaje puede hacer doble salto
 
 func _physics_process(delta):
+	animaciones()
 	# Detectar movimiento horizontal
 	velocity.x = 0
 	if Input.is_action_pressed("move_right"):
@@ -34,3 +35,17 @@ func _physics_process(delta):
 	# Resetear el doble salto cuando el personaje está en el suelo
 	if is_on_floor():
 		can_double_jump = true
+
+#Animaciones
+func animaciones():
+	if is_on_floor():
+		if velocity.x !=0:
+			$AnimatedSprite2D.scale.x = 1*sign(velocity.x)
+			$AnimatedSprite2D.play("run")
+		else:
+			$AnimatedSprite2D.play("idle")
+	else:
+		if velocity.y < 0:
+			$AnimatedSprite2D.play("jump")
+		else:
+			$AnimatedSprite2D.play("fall")
